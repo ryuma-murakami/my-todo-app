a131f3da1729d9bf9617b511c0be3f5954eeedab
ryuma-murakami <241606278+ryuma-murakami@users.noreply.github.com>
2025-11-11 23:45:49 +0900

    feat: add task creation feature

---
+++ src/components/CreateTaskForm.jsx
@@ -0,0 +1,40 @@
+import { Plus } from 'lucide-react';
+import { useRef } from 'react';
+
+export function CreateTaskForm({ onSubmit }) {
+  const inputRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputRef.current) {
+      return;
+    }
+
+    const inputValue = inputRef.current?.value.trim();
+    if (!inputValue) {
+      return;
+    }
+
+    onSubmit(inputValue);
+
+    inputRef.current.value = '';
+  };
+
+  return (
+    <form className="flex gap-0.5" onSubmit={handleSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="新しいタスクを入力してください"
+        className="grow rounded-s border border-gray-300 p-2 bg-white"
+      />
+      <button
+        type="submit"
+        className="rounded-e bg-blue-600 p-2 transition-colors hover:bg-blue-800 disabled:cursor-not-allowed disabled:bg-gray-400"
+        aria-label={'タスクを作成する'}
+      >
+        <Plus className="text-white" />
+      </button>
+    </form>
+  );
+}
